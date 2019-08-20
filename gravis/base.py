ddd226d7a76c532dd4337b28887703807047b15f
Ilya Chistyakov <ilchistyakov@gmail.com>
2019-08-21 02:53:59 +0300

    draw pass nodes

--- gravis/base.py
+++ gravis/base.py
@@ -4,151 +4,12 @@ from collections import defaultdict
 from contextlib import ContextDecorator
 from enum import Enum
 from io import StringIO
-from typing import Any, NamedTuple, List, Tuple
+from typing import NamedTuple, List, Tuple, Set
 
 __all__ = (
     'Node',
     'DebugContext',
 )
-
-
-class Direction(Enum):
-    backward = False
-    forward = True
-
-
-class NodeName(NamedTuple):
-    label: str
-    id: str
-
-    OPERATOR_MAP = {
-        'le': '<=',
-        'add': '+',
-        'sub': '-',
-    }
-
-    @staticmethod
-    def resolve_operator(value):
-        return NodeName.OPERATOR_MAP.get(value, value)
-
-    @staticmethod
-    def create(node: 'Node') -> 'NodeName':
-        from . import nodes
-
-        if isinstance(node, Node):
-            if isinstance(node, nodes.Constant):
-                label = str(node.value)
-            elif isinstance(node, nodes.If):
-                label = NodeName.resolve_operator(node.operator.__name__)
-            elif isinstance(node, nodes.Operator):
-                label = NodeName.resolve_operator(node.operator.__name__)
-            else:
-                label = repr(node)
-        else:
-            label = repr(node)
-
-        uuid = hex(id(node))[2:]
-        return NodeName(label=label, id=uuid)
-
-
-class Iteration(NamedTuple):
-    src: NodeName
-    dst: NodeName
-    args: Tuple[NodeName]
-    direction: Direction
-
-    @property
-    def style(self):
-        return 'solid' if self.direction == Direction.forward else 'dotted'
-
-    @property
-    def label_args(self):
-        if self.args:
-            return ', '.join(str(arg) for arg in self.args)
-        return ''
-
-    def render(self, step):
-        label_args = self.label_args
-        if label_args:
-            label = r'[{}]\n{}'.format(step, label_args)
-        else:
-            label = '[{}]'.format(step)
-        return (
-            '\t"{src}" -> "{dst}" '
-            '[label="{label}";style={style}];'
-            '\n'.format(
-                src=self.src.id,
-                dst=self.dst.id,
-                label=label,
-                style=self.style,
-            )
-        )
-
-
-class DebugContext(ContextDecorator):
-    STACK = []
-
-    def __init__(self):
-        self.iterations: List[Iteration] = []
-
-    def create_digraph(self):
-        stream = StringIO()
-        stream.write('digraph {\n')
-
-        for step, iteration in enumerate(self.iterations[1:]):
-            stream.write(iteration.render(step + 1))
-
-        nodes = set()
-        for iteration in self.iterations[1:]:
-            nodes.add(iteration.src)
-            nodes.add(iteration.dst)
-
-        for node in nodes:
-            stream.write('\t"{}" [label="{}"];\n'.format(node.id, node.label))
-
-        forwards = defaultdict(list)
-        for iteration in self.iterations[1:]:
-            if iteration.direction == Direction.forward:
-                forwards[iteration.src].append(iteration.dst)
-
-        rank = defaultdict(list)
-        for n, node in enumerate(nodes):
-            level = get_level(node, forwards)  # TODO: optimize
-            rank[level].append(node)
-
-        for nodes in rank.values():
-            if len(nodes) > 1:
-                stream.write(
-                    '\t{{rank=same {}}}\n'.format(
-                        ' '.join(
-                            '"{}"'.format(node.id)
-                            for node in nodes
-                        )
-                    )
-                )
-
-        stream.write('}\n')
-        return stream.getvalue()
-
-    def __enter__(self):
-        self.STACK.append(self)
-        return self
-
-    def __exit__(self, *exc):
-        self.STACK.pop()
-
-    @classmethod
-    def current(cls):
-        return cls.STACK[-1] if cls.STACK else None
-
-
-def get_level(node, forwards):
-    if not forwards:
-        return 0
-    levels = [0]
-    for node in forwards[node]:
-        levels.append(get_level(node, forwards))
-    return 1 + max(levels)
 
 
 def method_logger(func):
@@ -210,3 +71,186 @@ class Node(metaclass=NodeMeta):
     @abc.abstractmethod
     def activate_me(self, dst_node):
         pass
+
+
+class Direction(Enum):
+    backward = False
+    forward = True
+
+
+class NodeName(NamedTuple):
+    label: str
+    id: str
+    node: Node
+
+    OPERATOR_MAP = {
+        'le': '<=',
+        'add': '+',
+        'sub': '-',
+    }
+
+    @staticmethod
+    def resolve_operator(value):
+        return NodeName.OPERATOR_MAP.get(value, value)
+
+    @staticmethod
+    def create(node: Node) -> 'NodeName':
+        from . import nodes
+
+        if isinstance(node, nodes.Constant):
+            label = str(node.value)
+        elif isinstance(node, nodes.If):
+            label = NodeName.resolve_operator(node.operator.__name__)
+        elif isinstance(node, nodes.Operator):
+            label = NodeName.resolve_operator(node.operator.__name__)
+        else:
+            label = repr(node)
+
+        uuid = hex(id(node))[2:]
+        return NodeName(label=label, id=uuid, node=node)
+
+
+class Iteration(NamedTuple):
+    src: NodeName
+    dst: NodeName
+    args: Tuple[NodeName]
+    direction: Direction
+
+    @property
+    def style(self):
+        return 'solid' if self.direction == Direction.forward else 'dotted'
+
+    @property
+    def label_args(self):
+        if self.args:
+            return ', '.join(str(arg) for arg in self.args)
+        return ''
+
+    def render(self, step):
+        label_args = self.label_args
+        if label_args:
+            label = r'[{}]\n{}'.format(step, label_args)
+        else:
+            label = '[{}]'.format(step)
+        return (
+            '\t"{src}" -> "{dst}" '
+            '[label="{label}";style={style}];'
+            '\n'.format(
+                src=self.src.id,
+                dst=self.dst.id,
+                label=label,
+                style=self.style,
+            )
+        )
+
+
+class DebugContext(ContextDecorator):
+    STACK = []
+
+    def __init__(self):
+        self.iterations: List[Iteration] = []
+
+    def create_digraph(self):
+        stream = StringIO()
+        stream.write('digraph {\n')
+
+        all_links, all_nodes = collect_links(self.iterations[0].dst.node)
+        activated_links = {
+            (
+                (iteration.src.node, iteration.dst.node)
+                if id(iteration.src.node) > id(iteration.dst.node)
+                else (iteration.dst.node, iteration.src.node)
+            )
+            for iteration in self.iterations[1:]
+        }
+        pass_links = all_links - activated_links
+
+        pass_links = {
+            (NodeName.create(src), NodeName.create(dst))
+            for src, dst in pass_links
+        }
+        for src, dst in pass_links:
+            stream.write((
+                '\t"{src}" -> "{dst}" [arrowhead=none];'
+                '\n'.format(
+                    src=src.id,
+                    dst=dst.id,
+                )
+            ))
+
+        for step, iteration in enumerate(self.iterations[1:]):
+            stream.write(iteration.render(step + 1))
+
+        nodes = set()
+        for iteration in self.iterations[1:]:
+            nodes.add(iteration.src)
+            nodes.add(iteration.dst)
+        for src, dst in pass_links:
+            nodes.add(src)
+            nodes.add(dst)
+
+        for node in nodes:
+            stream.write('\t"{}" [label="{}"];\n'.format(node.id, node.label))
+
+        forwards = defaultdict(list)
+        for iteration in self.iterations[1:]:
+            if iteration.direction == Direction.forward:
+                forwards[iteration.src].append(iteration.dst)
+
+        rank = defaultdict(list)
+        for n, node in enumerate(nodes):
+            level = get_level(node, forwards)  # TODO: optimize
+            rank[level].append(node)
+
+        for nodes in rank.values():
+            if len(nodes) > 1:
+                stream.write(
+                    '\t{{rank=same {}}}\n'.format(
+                        ' '.join(
+                            '"{}"'.format(node.id)
+                            for node in nodes
+                        )
+                    )
+                )
+
+        stream.write('}\n')
+        return stream.getvalue()
+
+    def __enter__(self):
+        self.STACK.append(self)
+        return self
+
+    def __exit__(self, *exc):
+        self.STACK.pop()
+
+    @classmethod
+    def current(cls):
+        return cls.STACK[-1] if cls.STACK else None
+
+
+def get_level(node, forwards):
+    if not forwards:
+        return 0
+    levels = [0]
+    for node in forwards[node]:
+        levels.append(get_level(node, forwards))
+    return 1 + max(levels)
+
+
+def collect_links(start_node: Node):
+    pass_nodes = set()
+    links = set()
+    coming_nodes = {start_node}
+
+    while coming_nodes:
+        current = coming_nodes.pop()
+        pass_nodes.add(current)
+        linked_nodes = set(current.in_nodes + current.out_nodes) - pass_nodes
+        coming_nodes |= linked_nodes
+        for other in linked_nodes:
+            if id(current) > id(other):
+                links.add((current, other))
+            else:
+                links.add((other, current))
+
+    return links, pass_nodes
