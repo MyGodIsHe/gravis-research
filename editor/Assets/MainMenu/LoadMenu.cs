92cdcc2790787e0e8b44cc4b2bc56a7713abe325
Ilya Chistyakov <ilchistyakov@gmail.com>
2020-05-11 16:03:40 +0300

    done

---
+++ editor/Assets/MainMenu/LoadMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadMenu : MonoBehaviour
+{
+    public GameObject blankButton;
+    public GameObject scrollContent;
+
+    public void Activate()
+    {
+        gameObject.SetActive(true);
+        string[] filePaths = Directory.GetFiles(@"../examples/", "*.g");
+        foreach (var filePath in filePaths)
+        {
+            var gameObject = (GameObject)Instantiate(blankButton, scrollContent.transform);
+            var text = gameObject.GetComponentInChildren<TextMeshProUGUI>();
+            text.text = Path.GetFileNameWithoutExtension(filePath);
+            var button = gameObject.GetComponentInChildren<Button>();
+            button.onClick.AddListener(Deactivate);
+            var loader = gameObject.GetComponent<Loader>();
+            loader.filePath = filePath;
+        }
+    }
+
+    public void Deactivate()
+    {
+        gameObject.SetActive(false);
+        foreach (Transform child in scrollContent.transform)
+        {
+            GameObject.Destroy(child.gameObject);
+        }
+    }
+}
