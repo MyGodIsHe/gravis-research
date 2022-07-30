87359dac1155dee41a1753cc5a044f70733d05a9
DExquire <110129319+DExquire@users.noreply.github.com>
2022-07-31 00:04:50 +0300

    [+] Create Node & Change Color of selected Node (#21)
    * [+] Create Node & Change Color of selected Node
    
    * [*] Some mistakes corrected
    
    * All mistakes corrected!
    
    * [+] Unity.gitattributes file added
    
    * [*] file .gitattributes renamed
    
    * [*] file .gitattributes renamed

---
+++ editor/Assets/ChangeColor.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour
+{
+    
+    public MeshRenderer mesh;
+    public Color SelectColor;
+    public Color DeselectColor;
+    
+    private void Awake() {
+        mesh = gameObject.GetComponent<MeshRenderer>();
+        if((Vector4)DeselectColor == Vector4.zero)
+        {
+            DeselectColor = mesh.material.color;
+        }
+        mesh.material.color = DeselectColor;
+    }
+    private void OnEnable() {
+        ClickNode.instance.OnNodeSelected += ChangeColorOnSelected;
+        ClickNode.instance.OnNodeDeselected += ChangeColorOnDeselected;
+    }
+
+    void ChangeColorOnSelected()
+    {
+        if(ClickNode.instance.node == this.gameObject)
+        mesh.material.color = SelectColor;
+        else
+        mesh.material.color = DeselectColor;
+    }
+    
+    void ChangeColorOnDeselected()
+    {
+        if(ClickNode.instance.node != this.gameObject)
+        mesh.material.color = DeselectColor;
+    }
+}
--- editor/Assets/GraphManager.cs
+++ editor/Assets/GraphManager.cs
@@ -52,6 +52,7 @@ public class GraphManager : MonoBehaviour
             var position = (node.position + new Vector3(0, 0, offset)) * 2;
             position.y = -position.y;
             node.gameObject = Instantiate(cubeNode, position, Quaternion.identity);
+            node.gameObject.GetComponent<NodeLink>().nodeLink = node;
             volume.Add(node.gameObject);
             var textMesh = node.gameObject.GetComponentInChildren<TextMesh>();
             textMesh.text = node.text;
@@ -75,6 +76,7 @@ public class GraphManager : MonoBehaviour
 
 
         node.gameObject = Instantiate(cubeNode);
+        node.gameObject.GetComponent<NodeLink>().nodeLink = node;
         var textMesh = node.gameObject.GetComponentInChildren<TextMesh>();
         textMesh.text = node.text;
 
