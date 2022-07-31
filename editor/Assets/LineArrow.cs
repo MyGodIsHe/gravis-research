5a41d919430ebf5372ab8c7c4d622d440cc037ce
Denis <ulkiorrrra@gmail.com>
2022-07-31 18:59:02 +0300

    [+] arrow script created

---
+++ editor/Assets/LineArrow.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineArrow : MonoBehaviour
+{
+    public GameObject arrow;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
